665fadd99c7bf95ebc31842038445b0f6ded23f9
dasom222g <dasom228@gmail.com>
2025-08-31 17:13:36 +0900

    Fix: app host

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     # 개발 서버 실행
     uvicorn.run(
         "app.api:app",  # 앱 모듈 경로
-        host="localhost",  # 모든 IP에서 접근 가능
+        host="localhost",  # 로컬에서 접근 가능
         port=8000,  # 포트 번호
         reload=True,  # 코드 변경시 자동 재시작
     )
